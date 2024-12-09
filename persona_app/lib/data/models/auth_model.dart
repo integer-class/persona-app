87510b195763d0cc5dee06760c3b0987f54286ba
srikresna <srikresna383@gmail.com>
2024-12-09 20:15:32 +0700

    feat: refactor auth model

--- persona_app/lib/data/models/auth_model.dart
+++ persona_app/lib/data/models/auth_model.dart
@@ -1,92 +1,72 @@
+// auth_model.dart
 class AuthResponseModel {
   final String status;
   final String message;
-  final AuthData data;
+  final AuthData? data; // Make data optional
 
   AuthResponseModel({
     required this.status,
     required this.message,
-    required this.data,
+    required this.data, // Optional
   });
 
-  factory AuthResponseModel.fromJson(Map<String, dynamic> json) =>
-      AuthResponseModel(
-        status: json['status'],
-        message: json['message'],
-        data: AuthData.fromJson(json['data']),
-      );
+  factory AuthResponseModel.fromJson(Map<String, dynamic> json) {
+    return AuthResponseModel(
+      status: json['status'] ?? '',
+      message: json['message'] ?? '',
+      data: AuthData.fromJson(json['data']),
+    );
+  }
 
-  Map<String, dynamic> toJson() => {
-        'status': status,
-        'message': message,
-        'data': data.toJson(),
-      };
+  Map<String, dynamic> toJson() {
+    return {
+      'status': status,
+      'message': message,
+      'data': data?.toJson(), // Check if data is null
+    };
+  }
 }
 
 class AuthData {
-  final String token;
-  final int userId;
-  final String email;
+    final String token;
+    final User? user;
 
-  AuthData({
-    required this.token,
-    required this.userId,
-    required this.email,
-  });
+    AuthData({
+        required this.token,
+        required this.user,
+    });
 
-  factory AuthData.fromJson(Map<String, dynamic> json) => AuthData(
-        token: json['token'],
-        userId: json['userId'],
-        email: json['email'],
-  );
+    factory AuthData.fromJson(Map<String, dynamic> json) => AuthData(
+        token: json["token"],
+        user: User.fromJson(json["user"]),
+    );
 
-  Map<String, dynamic> toJson() => {
-        'token': token,
-        'userId': userId,
-        'email': email,
-      };
+    Map<String, dynamic> toJson() => {
+        "token": token,
+        "user": user?.toJson(),
+    };
 }
 
-class RegisterData {
-  final UserData user;
-  final String token;
+class User {
+    final int id;
+    final String username;
+    final String email;
 
-  RegisterData({
-    required this.user,
-    required this.token,
-  });
+    User({
+        required this.id,
+        required this.username,
+        required this.email,
+    });
 
-  factory RegisterData.fromJson(Map<String, dynamic> json) => RegisterData(
-    user: UserData.fromJson(json['user']),
-    token: json['token'],
-  );
+    factory User.fromJson(Map<String, dynamic> json) => User(
+        id: json["id"],
+        username: json["username"],
+        email: json["email"],
+    );
 
-  Map<String, dynamic> toJson() => {
-    'user': user.toJson(),
-    'token': token,
-  };
-}
-
-class UserData {
-  final int id;
-  final String username;
-  final String email;
-
-  UserData({
-    required this.id,
-    required this.username, 
-    required this.email,
-  });
-
-  factory UserData.fromJson(Map<String, dynamic> json) => UserData(
-    id: json['id'],
-    username: json['username'],
-    email: json['email'], 
-  );
-
-  Map<String, dynamic> toJson() => {
-    'id': id,
-    'username': username,
-    'email': email,
-  };
+    Map<String, dynamic> toJson() => {
+        "id": id,
+        "username": username,
+        "email": email,
+    };
 }
